79b99ef7383c03df298a7478695f5c6ae57d9c04
Blanca Ley <blanca.ley@outlook.com>
2022-05-16 13:49:10 +0200

    Send form info to server

---
+++ public/main.js
@@ -0,0 +1,25 @@
+const loginForm = document.getElementById("login");
+const userInput = document.getElementById("user");
+const passInput = document.getElementById("pass");
+const welcomeMsg = document.getElementById("welcomeMsg");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const res = await fetch("/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      user: userInput.value,
+      pass: passInput.value
+    })
+  });
+
+  const data = await res.json();
+
+  if (data.user) {
+    welcomeMsg.innerText = `Welcome ${data.user}`;
+    loginForm.hidden = true;
+  }
+})
