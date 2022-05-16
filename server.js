f75c4abce288fc0760ceb40d25db6acfc5d68f19
Blanca Ley <blanca.ley@outlook.com>
2022-05-16 13:16:56 +0200

    Handle session cookies with express-session middleware

--- server.js
+++ server.js
@@ -7,4 +7,13 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.json());
 
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'shhhh, very secret',
+  cookie: {
+    maxAge: 5 * 60 * 1000 // 5 minutes
+  }
+ }));
+
 app.listen(port, () => console.log(` Listening on port ${port}`))
